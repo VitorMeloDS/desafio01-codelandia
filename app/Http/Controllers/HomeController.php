b276cfec6096e061b6fce41dba79892f0a6bf53b
Vitor Melo da Silva <vmsvitor20@gmail.com>
2022-09-16 20:19:17 -0300

    feat: layout page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,5 +6,26 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+  public function index()
+  {
+    $posts = (object) [
+      'var1' => (object) [
+        'data' => '16/09/2022',
+        'titulo' => 'testando',
+        'texto' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti labore omnis facere molestiae asperiores corrupti, quasi magni, sit et laborum, dignissimos reiciendis ad atque libero iste sint error ex. Molestias.'
+      ],
+      'var2' => (object) [
+        'data'   => '17/09/2022',
+        'titulo' => 'testando2',
+        'texto'  => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti labore omnis facere molestiae asperiores corrupti, quasi magni, sit et laborum, dignissimos reiciendis ad atque libero iste sint error ex. Molestias.'
+      ],
+      'var3' => (object) [
+        'data'   => '18/09/2022',
+        'titulo' => 'testando3',
+        'texto'  => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti labore omnis facere molestiae asperiores corrupti, quasi magni, sit et laborum, dignissimos reiciendis ad atque libero iste sint error ex. Molestias.'
+      ],
+    ];
+
+    return view('welcome', ['posts' => $posts]);
+  }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,40 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-    </head>
-    <body class="antialiased">
-        @foreach ($ as )
-            
-        @endforeach
-    </body>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  <link rel="shortcut icon" href="../../public/img/blog.png" type="image/x-icon">
+  <title>Laravel</title>
+</head>
+
+<body class="antialiased">
+  <header class="bg-primary bg-gradient text-white ">
+    <nav class="container">
+      <div class="container pt-5 mx-xxl-5 d-flex justify-content-between">
+        <p class="mx-5">Codelândia</p>
+        <p class="mx-5">Blog</p>
+      </div>
+      <div class="d-flex mx-xl-5 py-3 mb-3 justify-content-center text-center">
+        <input class="form-control w-50 h-2" type="search" placeholder="Pesquisar" id="floatingPassword" aria-label="Pesquisar">
+      </div>
+    </nav>
+  </header>
+
+  <div class="container">
+    @foreach ($posts as $post)
+      <div class="bg-white my-5 py-3 px-xl-5">
+        <div class="d-flex justify-content-between">
+          <p>{{ $post->data }}</p>
+          <img src="{{url('../../public/img/heart.png')}}" alt="curtir">
+        </div>
+        <h3>{{ $post->titulo }}</h3>
+        <p>{{ $post->texto }}</p>
+      </div>
+    @endforeach
+  </div>
+
+</body>
+
 </html>
